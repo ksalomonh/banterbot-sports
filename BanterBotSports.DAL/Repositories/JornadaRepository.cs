5c8936fc823c3ae6d98918ee3aba550aca79ed87
ksalomonh <ksalomonh@users.noreply.github.com>
2026-03-24 12:09:42 -0600

    feat(dal): add concrete repository implementations for all 5 aggregates

---
+++ BanterBotSports.DAL/Repositories/JornadaRepository.cs
@@ -0,0 +1,53 @@
+using BanterBotSports.DAL.Repositories.Interfaces;
+using BanterBotSports.Entities;
+using BanterBotSports.Entities.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace BanterBotSports.DAL.Repositories;
+
+public class JornadaRepository : IJornadaRepository
+{
+    private readonly AppDbContext _context;
+
+    public JornadaRepository(AppDbContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+        _context = context;
+    }
+
+    public async Task<Jornada?> GetByIdAsync(int id)
+        => await _context.Jornadas.FindAsync(id);
+
+    public async Task<Jornada?> GetByIdWithDetailsAsync(int id)
+        => await _context.Jornadas
+            .Include(j => j.Torneo)
+            .Include(j => j.Partidos)
+            .Include(j => j.PrediccionesJornada)
+            .FirstOrDefaultAsync(j => j.Id == id);
+
+    public async Task<IReadOnlyList<Jornada>> GetAllAsync()
+        => await _context.Jornadas.ToListAsync();
+
+    public async Task<IReadOnlyList<Jornada>> GetByTorneoIdAsync(int torneoId)
+        => await _context.Jornadas
+            .Where(j => j.TorneoId == torneoId)
+            .OrderBy(j => j.Numero)
+            .ToListAsync();
+
+    public async Task<IReadOnlyList<Jornada>> GetByEstadoAsync(EstadoJornada estado)
+        => await _context.Jornadas
+            .Where(j => j.Estado == estado)
+            .ToListAsync();
+
+    public Task<Jornada> AddAsync(Jornada jornada)
+    {
+        _context.Jornadas.Add(jornada);
+        return Task.FromResult(jornada);
+    }
+
+    public Task UpdateAsync(Jornada jornada)
+    {
+        _context.Jornadas.Update(jornada);
+        return Task.CompletedTask;
+    }
+}
---
+++ BanterBotSports.DAL/Repositories/TorneoRepository.cs
@@ -0,0 +1,52 @@
+using BanterBotSports.DAL.Repositories.Interfaces;
+using BanterBotSports.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BanterBotSports.DAL.Repositories;
+
+public class TorneoRepository : ITorneoRepository
+{
+    private readonly AppDbContext _context;
+
+    public TorneoRepository(AppDbContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+        _context = context;
+    }
+
+    public async Task<Torneo?> GetByIdAsync(int id)
+        => await _context.Torneos.FindAsync(id);
+
+    public async Task<Torneo?> GetByIdWithDetailsAsync(int id)
+        => await _context.Torneos
+            .Include(t => t.ConfiguracionPremios)
+            .Include(t => t.Jornadas)
+            .Include(t => t.Participantes)
+            .FirstOrDefaultAsync(t => t.Id == id);
+
+    public async Task<IReadOnlyList<Torneo>> GetAllAsync()
+        => await _context.Torneos.ToListAsync();
+
+    public async Task<IReadOnlyList<Torneo>> GetByOrganizadorIdAsync(string organizadorId)
+        => await _context.Torneos
+            .Where(t => t.OrganizadorId == organizadorId)
+            .ToListAsync();
+
+    public Task<Torneo> AddAsync(Torneo torneo)
+    {
+        _context.Torneos.Add(torneo);
+        return Task.FromResult(torneo);
+    }
+
+    public Task UpdateAsync(Torneo torneo)
+    {
+        _context.Torneos.Update(torneo);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(Torneo torneo)
+    {
+        _context.Torneos.Remove(torneo);
+        return Task.CompletedTask;
+    }
+}
