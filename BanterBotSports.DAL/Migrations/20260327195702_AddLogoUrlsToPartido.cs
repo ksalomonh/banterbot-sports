0ee7d6f33591250a7ff98a59720ebca34ea19b76
ksalomonh <ksalomonh@users.noreply.github.com>
2026-03-27 13:57:26 -0600

    feat: add logo URL fields to Partido and wwwroot/images directory
    - Add LogoUrlLocal and LogoUrlVisitante nullable string properties to Partido entity (REQ-6)
    - Generate EF Core migration AddLogoUrlsToPartido (two ALTER TABLE ADD COLUMN statements)
    - Create wwwroot/images/.gitkeep to track the image hosting directory (REQ-7)

---
+++ BanterBotSports.DAL/Migrations/20260327195702_AddLogoUrlsToPartido.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BanterBotSports.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLogoUrlsToPartido : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrlLocal",
+                table: "Partidos",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrlVisitante",
+                table: "Partidos",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoUrlLocal",
+                table: "Partidos");
+
+            migrationBuilder.DropColumn(
+                name: "LogoUrlVisitante",
+                table: "Partidos");
+        }
+    }
+}
