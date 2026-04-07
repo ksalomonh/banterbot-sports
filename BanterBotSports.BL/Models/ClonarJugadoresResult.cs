ef720de7a1ca0b00d226d2464bcbc01139ee493f
ksalomonh <ksalomonh@users.noreply.github.com>
2026-04-07 16:34:38 -0600

    feat(entities): add ClonarJugadoresResult and TorneoResumen BL records

---
+++ BanterBotSports.BL/Models/ClonarJugadoresResult.cs
@@ -0,0 +1,4 @@
+namespace BanterBotSports.BL.Models;
+
+/// <summary>Result of a player-cloning operation between two torneos.</summary>
+public record ClonarJugadoresResult(int Clonados, int Omitidos);
