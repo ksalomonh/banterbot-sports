4c0da98c7dac1c533d2492e8b84d962d67437fe8
ksalomonh <ksalomonh@users.noreply.github.com>
2026-04-06 21:41:29 -0600

    fix(torneo): add missing ModelBinding using for ModelStateDictionary

--- BanterBotSports.Web/Controllers/TorneoController.cs
+++ BanterBotSports.Web/Controllers/TorneoController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BanterBotSports.Web.Controllers;
 
